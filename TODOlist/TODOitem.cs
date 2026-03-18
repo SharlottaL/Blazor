308ccfb899f0bdf88cb9df788ea832cd3dba26c9
SharlottaL <botanichkagigantika@gmail.com>
2026-03-18 16:00:54 +0300

    TODOlist almost DONE

---
+++ TODOlist/TODOitem.cs
@@ -0,0 +1,28 @@
+﻿namespace TODOlist
+{
+    public class TODOitem:IComparable<TODOitem>, IEqualityComparer<TODOitem>
+    {
+        public string Title { get; set; }
+        public bool DONE { get; set; }
+        public static bool operator ==(TODOitem left, TODOitem right)
+        {
+            return left.Title == right.Title;
+        }
+        public static bool operator !=(TODOitem left, TODOitem right)
+        {
+            return left.Title != right.Title;
+        }
+        public int CompareTo(TODOitem other)
+        {
+            return this.Title.CompareTo(other.Title);
+        }
+        public bool Equals(TODOitem left, TODOitem right)
+        {
+            return left.Title == right.Title;
+        }
+        public int GetHashCode(TODOitem other)
+        {
+            return HashCode.Combine(Title);
+        }
+    }
+}
