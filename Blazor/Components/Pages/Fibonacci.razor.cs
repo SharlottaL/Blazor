b63210d2321b87046dcb7b583bd62581aa40a6f2
SharlottaL <botanichkagigantika@gmail.com>
2026-03-18 13:55:11 +0300

    Power & Fibonacci DONE

---
+++ Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+        int number;
+        List<BigInteger> results = new List<BigInteger>();
+    
+        void Calculate()
+        {
+            results = new List<BigInteger>();
+            if (number <= 0) return;
+
+            if (number >= 1) results.Add(0);
+            if (number >= 2) results.Add(1);
+
+            for (int i = 2; i <= number; i++)
+            {
+                results.Add(results[i - 1] + results[i - 2]);
+            }
+        }
+    }
+}
---
+++ Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        long a;
+        long n;
+        long result;
+        List<long> results = new List<long>();
+        void Calculate()
+        {
+            result = 1;
+            results = new List<long>();
+            for (int i = 1; i <= n; i++)
+            {
+                result *= a;
+                results.Add(result);
+            }
+        }
+    }
+}
