4e2ab3b5ed687f99454cf750166d302eac256a97
SharlottaL <botanichkagigantika@gmail.com>
2026-04-13 13:01:56 +0300

    Student && Teacher Photo DONE

--- Academy2/Models/Student.cs
+++ Academy2/Models/Student.cs
@@ -1,28 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Academy2.Models
 {
-    public class Student
+    public class Student:Human
     {
         [Key]
         public int stud_id { get; set; }
         [Required]
-        public string last_name { get; set; }
-        [Required]
-        public string first_name { get; set; }
-        public string? middle_name { get; set; }
-        [Required]
-        [DataType(DataType.Date)]
-        public DateOnly birth_date { get; set; }
-        public string? email { get; set; }
-        public string? phone { get; set; }
-        [Column(TypeName = "image")]
-        public byte[]? photo { get; set; }
-        [Required]
         [ForeignKey(nameof(Group))]
         public int group { get; set; }
         /////////////////////////NP///////
-       
-        public Group ?Group { get; set; }
+
+        public Group? Group { get; set; }
     }
 }
